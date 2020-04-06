0bfba599b3276559baa043ca25d8f98aa940ebba
nandadesu <43083923+nandadesu@users.noreply.github.com>
2020-04-06 10:05:22 +0700

    pull

---
+++ Web_Lanjut/Jobsheet9/resources/views/profil.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Profil Saya</title>
+    </head>
+    <body>
+        <h1>Profil Saya</h1>
+        <p>Perkenalkan Nama saya Gusti Ananda, saya seorang mahasiswa POLINEMA JURUSAN TI</p>
+    </body>
+</html>
